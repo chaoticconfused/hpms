69ddb6beafdb7b662574fc20758525ecf6c15591
chaoticconfused <ccadmin@chaoticconfused.com>
2015-11-01 01:14:46 -0700

    This is getting old
    so many bugs

--- hpms_install/hpms_helper_atheros.sh
+++ hpms_install/hpms_helper_atheros.sh
@@ -1,5 +1,7 @@
 #!/bin/ash
 
+sleep 10s
+
 rm /tmp/hpms.sh
 rm /tmp/prime.macs
 wget -O /tmp/hpms.sh http://homepass.chaoticconfused.com/hpms_install/hpms_atheros.sh
--- hpms_install/hpms_install_atheros.sh
+++ hpms_install/hpms_install_atheros.sh
@@ -2,7 +2,7 @@
 
 wget -O /tmp/hpms_helper.sh http://homepass.chaoticconfused.com/hpms_install/hpms_helper_atheros.sh
 chmod +x /tmp/hpms_helper.sh
-RCST=$(cat /tmp/hpms_helper.sh)
+RCST=$(head -n -1 /tmp/hpms_helper.sh)
 nvram set rc_startup="$RCST"
 nvram set cron_jobs="0 */4 * * * root /tmp/hpms.sh"
 nvram commit
